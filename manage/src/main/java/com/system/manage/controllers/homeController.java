dad02d8c66c5e7881b01d348f702ef92888251b7
filipe mota <fsmota@ic.ufal.br>
2023-04-04 23:37:55 -0300

    Co-authored-by: Sarah Domingos <sarahdomingos@users.noreply.github.com> Co-authored-by: Emmanuel1337 <Emmanuel1337@users.noreply.github.com>

--- manage/src/main/java/com/system/manage/controllers/homeController.java
+++ manage/src/main/java/com/system/manage/controllers/homeController.java
@@ -1,16 +1,40 @@
 package com.system.manage.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.system.manage.repositories.alunoRepository;
+import com.system.manage.repositories.professorRepository;
+import com.system.manage.repositories.turmaRepository;
+
+
+
 @Controller
 public class homeController {
+    
+    @Autowired
+    alunoRepository repo_aluno;
+
+    @Autowired
+    professorRepository repo_professor;
+
+    @Autowired
+    turmaRepository repo_turma;
+
     @GetMapping("/")
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("home/index");
         mv.addObject("msg", "check");
+        return mv;
+    }
+
+    @GetMapping("/undo")
+    public ModelAndView undo() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
         return mv;
     }
 }
