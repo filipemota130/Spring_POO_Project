1c4b5dfce7487fc7bb0432eb01ce5c2b252eaf6d
filipe mota <fsmota@ic.ufal.br>
2023-04-11 23:23:00 -0300

    fix: trying to create a non-existing page

--- manage/src/main/java/com/system/manage/controllers/homeController.java
+++ manage/src/main/java/com/system/manage/controllers/homeController.java
@@ -3,13 +3,12 @@ package com.system.manage.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.http.HttpStatus;
 import com.system.manage.repositories.alunoRepository;
 import com.system.manage.repositories.professorRepository;
 import com.system.manage.repositories.turmaRepository;
-
-
 
 @Controller
 public class homeController {
@@ -32,6 +31,13 @@ public class homeController {
 
     @GetMapping("/undo")
     public ModelAndView undo() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        return mv;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView getHome() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("home/index");
         return mv;
